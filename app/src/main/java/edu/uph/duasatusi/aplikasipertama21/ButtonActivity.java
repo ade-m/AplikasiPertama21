2f178bd807d77dc5b830f70f767bb076cb8c5745
ade-m <66313509+ade-m@users.noreply.github.com>
2024-02-05 18:13:15 +0700

    intent

--- app/src/main/java/edu/uph/duasatusi/aplikasipertama21/ButtonActivity.java
+++ app/src/main/java/edu/uph/duasatusi/aplikasipertama21/ButtonActivity.java
@@ -2,6 +2,7 @@ package edu.uph.duasatusi.aplikasipertama21;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,7 +19,15 @@ public class ButtonActivity extends AppCompatActivity {
         supabutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //catat di log
                 Log.d("BUTTONS", "Buttun supa di klik");
+                //pindah dari Buttonactivity ke checkRadioActivity
+                Intent intent =
+                        new Intent(ButtonActivity.this,
+                                checkRadioActivity.class);
+                startActivity(intent);
+
+
             }
         });
 
